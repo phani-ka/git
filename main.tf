5f76d2d433f133d91130bdba480ce7b734eb0925
root <root@localhost.localdomain>
2022-11-07 11:08:57 +0530

    added git repo

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.0"
+  }
+}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name = "az-104"
+  location = "East US"
+}
+
