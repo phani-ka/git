7a7c30db2d89a58ab0a708ab31913c19a0865bd2
azuredevops <kphani7@outlook.com>
2022-11-10 18:22:51 +0530

    added nic

--- main.tf
+++ main.tf
@@ -11,14 +11,14 @@ provider "azurerm" {
   features {}
 }
 
-variable rg_name {}
-variable vpc_name {}
-variable az-security-group {}
-variable env_prefix {}
-variable avail_zone {}
-variable vpc_cid_block {}
-variable subnet_cidr_block {}
-variable az_location {}
+variable "rg_name" {}
+variable "vpc_name" {}
+variable "az-security-group" {}
+variable "env_prefix" {}
+variable "avail_zone" {}
+variable "vpc_cid_block" {}
+variable "subnet_cidr_block" {}
+variable "az_location" {}
 
 resource "azurerm_resource_group" "example" {
   name     = var.rg_name
@@ -44,6 +44,18 @@ resource "azurerm_network_security_group" "example" {
   tags = {
     Name : "${var.env_prefix}-${var.az-security-group}"
     environment = "${var.env_prefix}"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "${var.rg_name}-nic"
+  location            = var.avail_zone
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_virtual_network.example.subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
